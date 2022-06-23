73d36e911390733cd21141735d48a4aee62ce4e2
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-06-23 15:17:53 +0100

    Update title

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const TitleBar = ({handleSelectChange, genres}) =>{
+const TitleBar = ({ handleSelectChange, genres }) => {
   return (
     <div>
-      <h1>Hit Parade</h1>
+      <h1>CodeClan Hit Parade</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
